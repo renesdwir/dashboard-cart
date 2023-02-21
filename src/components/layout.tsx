22e7996ddab42f88aaedbdbaefe31801675424e8
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-02-21 22:45:16 +0700

    layout refactor

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,21 +1,41 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import Sidebar from "./sidebar";
 import styled from "styled-components";
 
 const Container = styled.div`
-  width: 100vw;
-  display: flex;
+  background: white;
 `;
-const Content = styled.div`
-  background: red;
-  flex-grow: 1;
-  padding: 10px;
+const Content = styled.div<{ isOpen: boolean }>`
+  width: ${(props) =>
+    props.isOpen ? "calc(100vw - 280px)" : "calc(100vw - 75px)"};
+  transition: width 0.3s ease-in-out;
+  background: white;
+  margin-left: ${(props) => (props.isOpen ? "280px" : "75px")};
+  float: right;
 `;
 const Layout = ({ children }: { children: ReactNode }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth <= 768) setIsOpen(false);
+    else setIsOpen(true);
+    const handleResize = () => {
+      if (window.innerWidth <= 768) setIsOpen(false);
+      else setIsOpen(true);
+    };
+    if (typeof window !== "undefined") {
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, []);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <Container>
-      <Sidebar />
-      <Content>{children}</Content>
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
+      <Content isOpen={isOpen}>{children}</Content>
     </Container>
   );
 };
